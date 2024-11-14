e241a1588a7624a7ca11787b192a69ac560dca68
tyler0611 <berlity0611@gmail.com>
2024-11-14 10:42:54 -0600

    Initial Commit

---
+++ Final Project V2/Final_Project_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_V2App.swift
+//  Final Project V2
+//
+//  Created by Tyler Berlin on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
