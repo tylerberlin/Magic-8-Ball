9270ad7a3bef36d1806de52542f903b51d28aa00
tyler0611 <berlity0611@gmail.com>
2024-11-21 10:16:06 -0600

    updaed

---
+++ Final Project V2/EightBall.swift
@@ -0,0 +1,18 @@
+//
+//  EightBall.swift
+//  Final Project V2
+//
+//  Created by Tyler Berlin on 11/18/24.
+//
+
+import SwiftUI
+
+struct EightBall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EightBall()
+}
---
+++ Reg8Ball.swift
@@ -0,0 +1,18 @@
+//
+//  Reg8Ball.swift
+//  Final Project V2
+//
+//  Created by Tyler Berlin on 11/14/24.
+//
+
+import SwiftUI
+
+struct Reg8Ball: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Reg8Ball()
+}
