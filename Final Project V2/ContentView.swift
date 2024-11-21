7062d0118b37cada8289e5cdbd664c6bf3b18401
tyler0611 <berlity0611@gmail.com>
2024-11-21 10:18:16 -0600

    deleted mean8Ball

--- Final Project V2/ContentView.swift
+++ Final Project V2/ContentView.swift
@@ -9,16 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                VStack {
+                    H1CustomText(text:"Welcome to Magic 8Ball")
+                        .font(.title).bold()
+                    Image("Icon").resizable().frame(width: 150, height: 150)
+                    H2CustomText(text:"Pick your 8Ball style and let the magic begin!")
+                        .font(.title2).bold()
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    NavigationLink("Regular 8 Ball", destination: EightBall(phrase: "Regular Magic 8 Ball"))
+                        .font(.title2)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .buttonStyle(CustomButtonStyle())
+                    NavigationLink("Mean 8 Ball", destination: EightBall(phrase: "Mean 8 Ball"))
+                        .font(.title2)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .buttonStyle(CustomButtonStyle())
+                    NavigationLink("Nice 8 Ball", destination: EightBall(phrase: "Nice 8 Ball"))
+                        .font(.title2)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .buttonStyle(CustomButtonStyle())
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct H1CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Futura Bold", size: 25))
+            .foregroundColor(.blue)
+    }
+}
+
+struct H2CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Futura Bold", size: 15))
+    }
+}
+
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 130)
+            .font(Font.custom("Futura Medium Italic", size: 20))
+            .padding()
+            .background(.blue).opacity(configuration.isPressed ? 0.0 : 1.0)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
 }
--- Final Project V2/EightBall.swift
+++ Final Project V2/EightBall.swift
@@ -2,17 +2,96 @@
 //  EightBall.swift
 //  Final Project V2
 //
-//  Created by Tyler Berlin on 11/18/24.
+//  Created by Tyler Berlin on 11/14/24.
 //
 
 import SwiftUI
 
 struct EightBall: View {
+    @State var randomValue = 0
+    @State var rotation = 0.0
+    @State var userQuestion = "" // User's question
+    let phrase: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Sets the background to black
+            Color.black.ignoresSafeArea()
+            VStack {
+                // Displays the phrase from ContentView
+                H1CustomText(text:(phrase))
+                    .font(.title).bold()
+                    .foregroundColor(Color.white)
+                    .padding(.top, 40)
+                
+                // Displays the 8-ball image with rotation animations
+                Image("pips \(randomValue)")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .padding()
+                    .padding(.top, 150)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                
+                Spacer()
+                    .padding(.bottom, 50)
+                
+                VStack {
+                    // Input field for the user to type their question
+                    TextField("Ask your question...", text: $userQuestion)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(.black)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                    
+                    //Button to trigger the "Ask Question" action. Only works if there is a question in the TextField
+                    Button("Ask Question") {
+                        if !userQuestion.isEmpty {
+                            chooseRandom(times: 3)
+                            withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                                rotation += 720 // Spins the 8-ball image
+                            }
+                        }
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                    //Makes the user hit the "Reset" button to ask another question after they have gotton a answer to the previus question.
+                    .disabled(randomValue != 0 || userQuestion.isEmpty)
+                    Button("Reset") {
+                        resetToOriginal()
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                }
+            }
+        }
+    }
+    //Resets Everthing
+    func resetToOriginal() {
+        randomValue = 0
+        rotation = 0
+        userQuestion = ""
+    }
+    
+    // Responses vary based on the mode selected by the user. If the user selects "Normal," the responses will mimic those of a traditional Magic 8-Ball. If they choose "Nice," only positive responses will be displayed. On the other hand, selecting "Mean" will generate negative or rude responses.
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                if phrase.contains("Mean") {
+                    randomValue = Int.random(in: 11...20)
+                } else if phrase.contains("Nice") {
+                    randomValue = Int.random(in: 1...10)
+                } else if phrase.contains("Regular") {
+                    randomValue = Int.random(in: 1...20)
+                } else if phrase.contains("Special"){
+                    randomValue = Int.random(in: 21...30)
+                }
+                chooseRandom(times: times - 1)
+            }
+        }
     }
 }
 
-#Preview {
-    EightBall()
+// Preview for testing
+struct EightBall_Previews: PreviewProvider {
+    static var previews: some View {
+        EightBall(phrase: "")
+    }
 }
